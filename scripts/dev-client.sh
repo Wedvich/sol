e83f91a631448d55ea87aeaec69d4fd5f46e0b68
Martin Wedvich <wedvich@gmail.com>
2025-11-15 12:49:07 +0100

    Scaffold build commands

--- scripts/dev-client.sh
+++ scripts/dev-client.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export NODE_ENV=development
+
 docker compose \
   -f ./database/docker-compose.yml \
   up -d --remove-orphans
--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export NODE_ENV=test
+
 docker compose \
   -f ./database/docker-compose.yml \
   -f ./server/docker-compose.yml \
